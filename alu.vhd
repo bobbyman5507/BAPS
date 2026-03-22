1f9bfe96a1b5efd15cd286897a3d88d1a1fce184
Anthony Rosati Jr <atrain0523@gmail.com>
2026-03-22 11:39:36 -0400

    Add ALU entity and structural architecture

---
+++ alu.vhd
@@ -0,0 +1,12 @@
+entity alu is
+  port(
+    A, B : in std_logic_vector(19 downto 0);
+    Y : out std_logic_vector(19 downto 0);
+  );
+end alu;
+
+  
+architecture structural of alu is 
+
+  
+end structural;
