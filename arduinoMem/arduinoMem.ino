434f3c24364ce705e0240b036a71200a9948a0f6
Anthony Rosati <atrain0523@gmail.com>
2026-03-29 12:35:43 -0400

    added pin setup for memory

--- arduinoMem/arduinoMem.ino
+++ arduinoMem/arduinoMem.ino
@@ -6,12 +6,24 @@ void setup()
   // writePin, always input
   pinMode(writePin, INPUT);
 
-  for (int i = 1; i <= 17; i++) {}
+  for (int i = 1; i <= 17; i++) {
     pinMode(i, INPUT);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // set pins for current cycle
+  w_en = digitalRead(writePin);
+  if (w_en == HIGH) {
+    for (int i = 0; i <= 17; i++){
+      pimMode(i, INPUT);
+    }
+  }
+
+  else {
+    for (int i = 0; i <= 17; i++){
+      pinMode(i, OUTPUT);
+    }
+  }
   
 }
