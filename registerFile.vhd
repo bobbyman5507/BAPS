cdcc85f41a5ee0499beb5af4273100b919c70b42
Bobby Orbin <robert.orbin@gmail.com>
2026-03-29 12:42:29 -0400

    Finished Dff and started Register File

---
+++ registerFile.vhd
@@ -0,0 +1,21 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity RegisterFile is
+    port 
+    (
+        regD : in std_logic_vector(2 downto 0);
+        regX : in std_logic_vector(2 downto 0);
+        regY : in std_logic_vector(2 downto 0);
+
+        
+
+
+
+       
+    );
+end RegisterFile;
+
+
+end rtl;
